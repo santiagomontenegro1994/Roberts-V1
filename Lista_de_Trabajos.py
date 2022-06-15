f3ba69c3935900aeb14d0d449657189f24f4d31a
PachyVc <pachycat94@gmail.com>
2022-06-15 17:15:02 -0300

    Agregando funcionalidades y configuranco base de datos

--- Lista_de_Trabajos.py
+++ Lista_de_Trabajos.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
+from PIL import ImageTk, Image
 
 
 class listaDeTrabajos():
@@ -15,12 +16,14 @@ class listaDeTrabajos():
     def __init__(self,window):
         self.wind=window
         self.wind.title("Lista de Trabajos")
-        self.wind.geometry("1024x800")
+        self.wind.geometry("1200x800")
         
         self.t1=tk.StringVar()
         self.t2=tk.StringVar()
         self.t3=tk.StringVar()
         self.t4=tk.StringVar()
+        self.t5=tk.StringVar()
+        self.t6=tk.StringVar()
         
         #Creando el contenedor
         wrapper1 = LabelFrame(window, text="Lista de Trabajos")
@@ -34,20 +37,35 @@ class listaDeTrabajos():
         #------------------------------Seccion de lista
         
         #Creando el treeview
-        self.tabla = ttk.Treeview(wrapper1, columns=(1,2,3,4,5,6), show="headings", height="6")
-        self.tabla.pack()
+        self.tabla = ttk.Treeview(wrapper1, columns=(1,2,3,4,5,6,7,8), show="headings")
+        self.style=ttk.Style(self.tabla)
+        self.style.configure('Treeview', rowheight=25)
         
+        self.tabla.pack()
         self.tabla.heading(1, text="Estado")
         self.tabla.heading(2, text="Fecha")
         self.tabla.heading(3, text="Cliente")
         self.tabla.heading(4, text="Trabajo")
         self.tabla.heading(5, text="Precio")
-        self.tabla.heading(6, text="Id Trabajo")
+        self.tabla.heading(6, text="Seña")
+        self.tabla.heading(7, text="Saldo")
+        self.tabla.heading(8, text="Id")
+        
+        #dando el ancho de las columnas
+        self.tabla.column('#1', width=70, anchor='c')
+        self.tabla.column('#2', width=70, anchor='c')
+        self.tabla.column('#3', width=300, anchor='c')
+        self.tabla.column('#4', width=300, anchor='c')
+        self.tabla.column('#5', width=70, anchor='c')
+        self.tabla.column('#6', width=70, anchor='c')
+        self.tabla.column('#7', width=70, anchor='c')
+        self.tabla.column('#8', width=50, anchor='c')
+        
         
         self.tabla.bind('<Double-1>', self.getrow)# al hacer doble click selecciona un cliente con los datos
         
         #lleno la lista de elementos
-        query="SELECT Estado, Fecha, Cliente, Trabajo, Precio, Id FROM Lista_de_Trabajos" 
+        query="SELECT Estado, Fecha, Cliente, Trabajo, Precio, Seña, Saldo, Id FROM Lista_de_Trabajos" 
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
         self.updateLista(rows)
@@ -65,30 +83,46 @@ class listaDeTrabajos():
         
         #------------Seccion de carga y modificacion de datos                     
 
-        self.lbl1 = Label(wrapper3, text="Cliente")
+        self.lbl1 = Label(wrapper3, text="Estado")
         self.lbl1.grid(row=1, column=0, padx=5, pady=3)
         self.ent1= Entry(wrapper3, textvariable=self.t1)
         self.ent1.grid(row=1, column=1, pady=3, columnspan=3, sticky= W+E)
+        self.lbl1 = Label(wrapper3, text="P / EP / L / E")
+        self.lbl1.grid(row=1, column=4, padx=5, pady=3)
+        
+        self.lbl1 = Label(wrapper3, text="Cliente")
+        self.lbl1.grid(row=2, column=0, padx=5, pady=3)
+        self.ent1= Entry(wrapper3, textvariable=self.t2)
+        self.ent1.grid(row=2, column=1, pady=3, columnspan=3, sticky= W+E)
         
         self.lbl2 = Label(wrapper3, text="Trabajo")
-        self.lbl2.grid(row=2, column=0, padx=5, pady=3)
-        self.ent2= Entry(wrapper3, textvariable=self.t2)
-        self.ent2.grid(row=2, column=1, pady=3, columnspan=3, sticky= W+E)
+        self.lbl2.grid(row=3, column=0, padx=5, pady=3)
+        self.ent2= Entry(wrapper3, textvariable=self.t3)
+        self.ent2.grid(row=3, column=1, pady=3, columnspan=3, sticky= W+E)
         
         self.lbl3 = Label(wrapper3, text="Precio")
-        self.lbl3.grid(row=3, column=0, padx=5, pady=3)
-        self.ent3= Entry(wrapper3, textvariable=self.t3)
-        self.ent3.grid(row=3, column=1, pady=3, columnspan=3, sticky= W+E)
+        self.lbl3.grid(row=4, column=0, padx=5, pady=3)
+        self.ent3= Entry(wrapper3, textvariable=self.t4)
+        self.ent3.grid(row=4, column=1, pady=3, columnspan=3, sticky= W+E)
+        
+        self.lbl3 = Label(wrapper3, text="Seña")
+        self.lbl3.grid(row=5, column=0, padx=5, pady=3)
+        self.ent3= Entry(wrapper3, textvariable=self.t5)
+        self.ent3.grid(row=5, column=1, pady=3, columnspan=3, sticky= W+E)
         
         self.ag_btn=Button(wrapper3, text= "Agregar", command= self.agregar)
-        self.ag_btn.grid(row=4, column=0, padx=5, pady=3)
+        self.ag_btn.grid(row=1, column=6, padx=5, pady=3)
         
         self.mod_btn=Button(wrapper3, text="Modificar", command=self.modificar)
-        self.mod_btn.grid(row=4, column=1, padx=5, pady=3)
+        self.mod_btn.grid(row=2, column=6, padx=5, pady=3)
         
         self.el_btn=Button(wrapper3, text="Eliminar", command= self.eliminar)
-        self.el_btn.grid(row=4, column=2, padx=5, pady=3)
+        self.el_btn.grid(row=3, column=6, padx=5, pady=3)
         
+        self.lim_btn=Button(wrapper3, text="Limpiar", command= self.limpiarc)
+        self.lim_btn.grid(row=4, column=6, padx=5, pady=3)
+        
+ 
         
 #---------------------------METODOS------------------------        
     
@@ -99,58 +133,78 @@ class listaDeTrabajos():
             self.tabla.delete(element)
         #consultando los datos
         for i in rows:
-            self.tabla.insert('', 'end', values=i)    
+            self.tabla.insert('', 'end', values=i) 
+    
+    def limpiarc(self):
+        self.t1.set('')
+        self.t2.set('')
+        self.t3.set('')
+        self.t4.set('')
+        self.t5.set('')
+        self.t6.set('')                        
         
     def search(self):
         q2=self.ent.get()
-        query= 'SELECT Estado, Fecha, Cliente, Trabajo, Precio, Id FROM Lista_de_Trabajos WHERE Cliente = ?'
+        query= 'SELECT Estado, Fecha, Cliente, Trabajo, Precio, Seña, Saldo, Id FROM Lista_de_Trabajos WHERE Cliente = ?'
         self.cursor.execute(query, [q2])
         rows = self.cursor.fetchall()
         self.updateLista(rows)
     
     def limpiar(self):
-        query = "SELECT Estado, Fecha, Cliente, Trabajo, Precio, Id FROM Lista_de_Trabajos"        
+        query = "SELECT Estado, Fecha, Cliente, Trabajo, Precio, Seña, Saldo, Id FROM Lista_de_Trabajos"        
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
         self.updateLista(rows)
     
-    def getrow(self, event): #selecciono los datos y los paso a las variables t1,t2 y t3
+    def getrow(self, event): #selecciono los datos y los paso a las variables t
         rowid = self.tabla.identify_row(event.y)
         item = self.tabla.item(self.tabla.focus())
-        self.t1.set(item['values'][2])
-        self.t2.set(item['values'][3])
-        self.t3.set(item['values'][4])
-        self.t4.set(item['values'][5])#variable que guarda el ID para eliminar
+        self.t1.set(item['values'][0])
+        self.t2.set(item['values'][2])
+        self.t3.set(item['values'][3])
+        self.t4.set(item['values'][4])
+        self.t5.set(item['values'][5])
+        self.t6.set(item['values'][7])#varible que guarda el ID para eliminar
         
     def agregar(self):
-        estado='Listo'
+        estado=self.t1.get()
         fecha='Hoy'
-        cliente=self.t1.get()
-        trabajo=self.t2.get()
-        precio=self.t3.get()
-        query='INSERT INTO Lista_de_Trabajos(Estado, Fecha, Cliente, Trabajo, Precio, Id) VALUES(?,?,?,?,?,NULL)' 
-        self.cursor.execute(query,(estado,fecha,cliente,trabajo,precio))
-        self.conn.commit()
-        messagebox.showinfo(message="Trabajo Agregado Correctamente!", title="Confirmacion")
-        self.limpiar()
-        
-    def modificar(self):
-        estado='Listo'
-        fecha='Hoy'
-        cliente=self.t1.get()
-        trabajo=self.t2.get()
-        precio=self.t3.get()
-        id=self.t4.get()
-        if messagebox.askyesno("Confirmar Modificacion","Estas Seguro de Modificar este trabajo?"):
-            query = "UPDATE Lista_de_Trabajos SET Estado=?, Fecha=?, Cliente=?, Trabajo=?, Precio=?, Id=? WHERE Id= ?" 
-            self.cursor.execute(query,(estado,fecha,cliente,trabajo,precio,id,id))
+        cliente=self.t2.get()
+        trabajo=self.t3.get()
+        precio=self.t4.get()
+        seña=self.t5.get()
+        if  len(estado) != 0 and len(cliente) !=0 and len(trabajo) !=0 and len(precio) !=0  and len(seña) !=0 :  #valido que no le pasen valores vacios
+            saldo= int(precio)-int(seña)
+            query='INSERT INTO Lista_de_Trabajos(Estado, Fecha, Cliente, Trabajo, Precio, Seña, Saldo, Id) VALUES(?,?,?,?,?,?,?,NULL)' 
+            self.cursor.execute(query,(estado,fecha,cliente,trabajo,precio,seña,saldo))
             self.conn.commit()
+            messagebox.showinfo(message="Trabajo Agregado Correctamente!", title="Confirmacion")
             self.limpiar()
         else:
-            return True    
+            messagebox.showinfo(message="No se puede agregra trabajo con datos vacios", title="Aviso!")
             
+    def modificar(self):
+        estado=self.t1.get()
+        fecha='Hoy'
+        cliente=self.t2.get()
+        trabajo=self.t3.get()
+        precio=self.t4.get()
+        seña=self.t5.get()
+        id=self.t6.get()
+        if  len(estado) != 0 and len(cliente) !=0 and len(trabajo) !=0 and len(precio) !=0 and len(seña) !=0:  #valido que no le pasen valores vacios
+            saldo= int(precio)-int(seña)
+            if messagebox.askyesno("Confirmar Modificacion","Estas Seguro de Modificar este trabajo?"):
+                query = "UPDATE Lista_de_Trabajos SET Estado=?, Fecha=?, Cliente=?, Trabajo=?, Precio=?, Seña=?, Saldo=?, Id=? WHERE Id= ?" 
+                self.cursor.execute(query,(estado,fecha,cliente,trabajo,precio,seña,saldo,id,id))
+                self.conn.commit()
+                self.limpiar()
+            else:
+                return True    
+        else:
+            messagebox.showinfo(message="No se puede modificar un trabajo con datos vacios", title="Aviso!")
+                
     def eliminar(self):
-        idLista= self.t4.get()
+        idLista= self.t6.get()
         if messagebox.askyesno("Confirma que quiere eliminar?", "Estas seguro de eliminar este Trabajo?"): # Mensaje para confirmar eliminacion
             query='DELETE FROM Lista_de_Trabajos WHERE Id = ?'
             self.cursor.execute(query,(idLista))
